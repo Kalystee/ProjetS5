7f25aa49988c724decbdd1483aa9430bec0331ef
Guillaume Thibault <guillaume.thibault@etudiant.univ-reims.fr>
2018-12-05 16:29:01 +0100

    Correction erreur de dossier

---
+++ MasterChef/Controlleur/Entity/Cuisine/PersonnelCuisine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controlleur.Entity.Cuisine
+{
+    public abstract class PersonnelCuisine
+    {
+        protected int Id { get; set; }
+
+        public PersonnelCuisine(int id)
+        {
+
+        }
+    }
+}
