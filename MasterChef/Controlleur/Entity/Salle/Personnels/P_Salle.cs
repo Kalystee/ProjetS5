148afd0b2d5ccb5409d5bc362bb08ede29317e9f
ADESWAEME <alexandra.deswaeme@viacesi.fr>
2018-12-05 16:47:10 +0100

    Update
    Création du personnels du restaurant dans la partie salle

---
+++ MasterChef/Controlleur/Entity/Salle/Personnels/P_Salle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controlleur.Entity.Salle.Personnels
+{
+    public abstract class P_Salle
+    {
+        protected int id;
+        public int Id
+        {
+            get
+            {
+                return this.id;  
+            }
+
+            set
+            {
+                this.id=value;
+            }
+        }
+        
+
+        public P_Salle()
+        {
+         
+        }
+    }
+}
