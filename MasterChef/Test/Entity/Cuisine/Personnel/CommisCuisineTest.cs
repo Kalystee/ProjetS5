966ff594e4405b835eb94815f11e33210a39ea03
Guillaume Thibault <guillaume.thibault@etudiant.univ-reims.fr>
2018-12-06 14:39:43 +0100

    Ustensile correctifé

---
+++ MasterChef/Test/Entity/Cuisine/Personnel/CommisCuisineTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Entity.Cuisine.Personnel
+{
+    [TestClass]
+    public class CommisCuisineTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
