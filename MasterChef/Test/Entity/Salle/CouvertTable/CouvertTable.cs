ddabe3a54a042986d9ced748c563d44a11dd4a6a
Vicso <Victor.sorrenti@viacesi.fr>
2018-12-06 13:53:56 +0100

    division class lavage

--- MasterChef/Test/Entity/Salle/CouvertTable/CouvertTable.cs
+++ MasterChef/Test/Entity/Salle/CouvertTable/CouvertTable.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Controlleur.Entity.Salle.CouvertTable
+namespace Test.Entity.Salle.CouvertTable
 {
-    public class CouvertTable
+    [TestClass]
+    public class CouvertTableTest
     {
+        [TestMethod]
+        public NotifyTest()
+        {
+
+        }
+        [TestMethod]
+        public ReactAssiettesTest()
+        {
+
+        }
+        [TestMethod]
+        public ReactVerresTest()
+        {
+
+        }
+        [TestMethod]
+        public ReactCouvertsTest()
+        {
+
+        }
+        [TestMethod]
+        public ReactTissusTest()
+        {
+
+        }
     }
 }
--- MasterChef/Test/Entity/Salle/CouvertTable/Tissus.cs
+++ MasterChef/Test/Entity/Salle/CouvertTable/Tissus.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Controlleur.Entity.Salle.CouvertTable
+namespace Test.Entity.Salle.CouvertTable
 {
-    public class Tissu
+    [TestClass]
+    public class TissusTest
     {
+        [TestMethod]
+        public OperationTissusTest()
+        {
+
+        }
     }
 }
