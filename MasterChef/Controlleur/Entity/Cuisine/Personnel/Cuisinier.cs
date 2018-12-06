3ff82636f015cca2e3bf5ee55a0627c8d49e4e7b
Guillaume Thibault <guillaume.thibault@etudiant.univ-reims.fr>
2018-12-06 08:55:57 +0100

    Modif Personnel Salle

--- MasterChef/Controlleur/Entity/Cuisine/Personnel/Cuisinier.cs
+++ MasterChef/Controlleur/Entity/Cuisine/Personnel/Cuisinier.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Controlleur.Entity.Cuisine.Plats;
 
 namespace Controlleur.Entity.Cuisine
 {
     public class Cuisinier
     {
+        public PlatBuilder Builder { get; set; }
     }
 }
