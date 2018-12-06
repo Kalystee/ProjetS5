d38b68bb1b15e1117abd85b1f42512564b8f5d62
sofna <45632019+sofna@users.noreply.github.com>
2018-12-06 15:24:53 +0100

    Test Machine Cuisson

---
+++ MasterChef/Test/Entity/Cuisine/Machines/MachineCuissonTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Entity.Cuisine.Machines
+{
+    [TestClass]
+    public class MachineCuissonTest
+    {
+       
+        {[TestMethod]
+        public void TestCuisson()
+        {
+            Machinecuisson c = new MachineCuisson();
+            Bouffe b = new Bouffe();
+
+            b.isNoncuisiner = true;
+
+            z.listAliment.Add(b);
+            z.Cuisson();
+            Assert.IsFalse(b.isNoncuisiner);
+        }
+    }
+    }
