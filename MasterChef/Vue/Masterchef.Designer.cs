0fd068514cac7da9131dfbabc39f9d69a903793a
Tombrunetti <tom.brunetti@viacesi.fr>
2018-12-07 14:48:26 +0100

    Add Winform
    View v1

---
+++ MasterChef/Vue/Masterchef.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Vue
+{
+    partial class Masterchef
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Masterchef));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.hour = new System.Windows.Forms.TextBox();
+            this.minute = new System.Windows.Forms.TextBox();
+            this.second = new System.Windows.Forms.TextBox();
+            this.day = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1078, 704);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(1118, 137);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 1;
+            this.Add.Text = "Add client";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1086, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1130, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "add client";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // hour
+            // 
+            this.hour.Location = new System.Drawing.Point(1112, 572);
+            this.hour.Name = "hour";
+            this.hour.Size = new System.Drawing.Size(26, 20);
+            this.hour.TabIndex = 4;
+            this.hour.Text = "12";
+            this.hour.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.hour.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // minute
+            // 
+            this.minute.Location = new System.Drawing.Point(1144, 572);
+            this.minute.Name = "minute";
+            this.minute.Size = new System.Drawing.Size(26, 20);
+            this.minute.TabIndex = 5;
+            this.minute.Text = "0";
+            this.minute.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.minute.TextChanged += new System.EventHandler(this.minute_TextChanged);
+            // 
+            // second
+            // 
+            this.second.Location = new System.Drawing.Point(1176, 572);
+            this.second.Name = "second";
+            this.second.Size = new System.Drawing.Size(26, 20);
+            this.second.TabIndex = 6;
+            this.second.Text = "0";
+            this.second.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.second.TextChanged += new System.EventHandler(this.second_TextChanged);
+            // 
+            // day
+            // 
+            this.day.Location = new System.Drawing.Point(1080, 572);
+            this.day.Name = "day";
+            this.day.Size = new System.Drawing.Size(26, 20);
+            this.day.TabIndex = 7;
+            this.day.Text = "0";
+            this.day.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.day.TextChanged += new System.EventHandler(this.day_TextChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(1090, 641);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(122, 45);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Value = 1000;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1131, 598);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1080, 598);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1182, 598);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(44, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Lunch",
+            "Dinner"});
+            this.comboBox1.Location = new System.Drawing.Point(1084, 510);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(66, 21);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1112, 414);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Setting";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Masterchef
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1234, 696);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.day);
+            this.Controls.Add(this.second);
+            this.Controls.Add(this.minute);
+            this.Controls.Add(this.hour);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Masterchef";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "MasterChef";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox hour;
+        private System.Windows.Forms.TextBox minute;
+        private System.Windows.Forms.TextBox second;
+        private System.Windows.Forms.TextBox day;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ MasterChef/Vue/Masterchef.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vue
+{
+    public partial class Masterchef : Form
+    {
+        public Masterchef()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            string nbr_client;
+            nbr_client = textBox1.Text;
+            MessageBox.Show(nbr_client);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int secondInt = Convert.ToInt32(second.Text);
+            if (secondInt < 59)
+            {
+                second.Text = Convert.ToString(secondInt + 1);
+            }
+            else
+            {
+                second.Text = "0";
+                int minuteInt = Convert.ToInt32(minute.Text);
+                if (minuteInt < 59)
+                {
+                    minute.Text = Convert.ToString(minuteInt + 1);
+                }
+                else
+                {
+                    minute.Text = "0";
+                    int hourInt = Convert.ToInt32(hour.Text);
+                    if (hourInt < 23)
+                    {
+                        hour.Text = Convert.ToString(hourInt + 1);
+                    }
+                    else
+                    {
+                        hour.Text = "0";
+                        int dayInt = Convert.ToInt32(day.Text);
+                        if (dayInt < 7)
+                        {
+                            day.Text = Convert.ToString(dayInt + 1);
+                        }
+                        else
+                        {
+                            day.Text = "0";
+                        }
+                    }
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void second_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void day_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            timer1.Interval = trackBar1.Value;
+        }
+
+        private void minute_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            timer1.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == "Lunch")
+            {
+                timer1.Stop();
+                second.Text = "0";
+                minute.Text = "0";
+                hour.Text = "12";
+                day.Text = "0";
+            }
+            if (comboBox1.SelectedItem == "Dinner")
+            {
+                timer1.Stop();
+                second.Text = "0";
+                minute.Text = "0";
+                hour.Text = "19";
+                day.Text = "0";
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == "Lunch")
+            {
+                second.Text = "0";
+                minute.Text = "0";
+                hour.Text = "12";
+                day.Text = "0";
+            }
+            if (comboBox1.SelectedItem == "Dinner")
+            {
+                second.Text = "0";
+                minute.Text = "0";
+                hour.Text = "19";
+                day.Text = "0";
+            }
+        }
+    }
+}
